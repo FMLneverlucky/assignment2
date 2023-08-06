6db9e1269bd8ff792a26dc9109a9bd100cab5951
FMLneverlucky <66565733+FMLneverlucky@users.noreply.github.com>
2023-08-06 16:57:39 +0800

    milestone commit: toggle visibility function
    clicking on button under subcontent page will collapse/ expand content underneath

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+/*--------------------------------code for subcontent switching-------------------------------------------------------------------*/
 const page1btn = document.querySelector("#page1btn");
 const page2btn = document.querySelector("#page2btn");
 const page3btn = document.querySelector("#page3btn");
 var allpages = document.querySelectorAll(".subcontent");
+/* allpages seem to be tied to content under subcontent class
+as in diff pages under subcontent class */
 
 console.log(allpages);
 
@@ -24,3 +27,41 @@ function show(pgNo) {
 page1btn.addEventListener("click", function () {show(1);} );
 page2btn.addEventListener("click", function () {show(2);} );
 page3btn.addEventListener("click", function () {show(3);} );
+/*------------------------------------------subcontent switching end------------------------------------------------------------- */
+
+/*------------------------------------------------code for collapsible content--------------------------------------------------- */
+const Med_Pg_1 = document.querySelector("#Med_Pg_1");
+const Med_Pg_2 = document.querySelector("#Med_Pg_2");
+const Med_Pg_3 = document.querySelector("#Med_Pg_3");
+var allCollapsibleContent = document.querySelectorAll(".collapsible-content");
+console.log(allCollapsibleContent);
+/*yes this is copy paste
+brain not working to figure how to do in another method or sth */
+
+/*anyways all collapsible pages open by default
+clicking header will collapse the section
+to do:
+change arrow pic to arrow up (content open by default)
+hide content when header clicked (collapse content under header)
+arrow img change to arrow down when content collapsed */
+function toggle_content_visibility(pageNum) {
+    let onepage =document.querySelector("#med-pg-" + pageNum);
+    if (onepage.style.display == "block")
+    {
+        onepage.style.display = "none";
+        return;
+    }
+    /*
+    if (onepage.style.display == "none")
+    {
+        onepage.style.display = "block";
+        return;
+    }
+    */
+   else{
+    onepage.style.display = "block";
+   }
+}
+Med_Pg_1.addEventListener("click", function () {toggle_content_visibility(1);} );
+Med_Pg_2.addEventListener("click", function () {toggle_content_visibility(2);} );
+Med_Pg_3.addEventListener("click", function () {toggle_content_visibility(3);} );
